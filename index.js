36d4f97c3cfaf04bf471e5136ba91cc76e0181e2
ruhul2003 <ruhul941020@gmail.com>
2026-06-26 15:29:08 +0600

    Added vercel.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://nexus-library-client.vercel.app"],
     credentials: true,
   }),
 );
